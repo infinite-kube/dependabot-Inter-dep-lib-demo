acabdd15236591a256956c88c5727c819183700d
infinite-kube <kcarrow@infinite.com>
2025-10-06 16:35:53 -0400

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const _ = require('lodash');
+
+const app = express();
+app.get('/', (_req, res) => {
+  res.json({ msg: 'Interdependent demo', lodash: _.VERSION });
+});
+
+if (require.main === module) {
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
+}
+
+module.exports = app;
